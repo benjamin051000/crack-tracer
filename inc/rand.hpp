759db242e050fd054e942da320ea5251e0db738f
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-09-19 13:29:53 -0400

    more stuffs

--- inc/rand.hpp
+++ inc/rand.hpp
@@ -11,8 +11,8 @@ public:
   [[nodiscard]] __m256 rand_in_range_256(float min, float max);
 
 private:
-  static thread_local __m256i rseed_vec = comptime::init_rseed_arr();
-  static thread_local uint32_t rseed = 0;
+  static constexpr thread_local __m256i rseed_vec = comptime::init_rseed_arr();
+  static constexpr thread_local uint32_t rseed = 0;
   const __m256i r_a = _mm256_set1_epi32((uint32_t)11035152453u);
   const __m256i r_b = _mm256_set1_epi32(12345u);
   const __m256i rand_max_vec = _mm256_set1_epi32(RAND_MAX);
--- inc/render.hpp
+++ inc/render.hpp
@@ -16,4 +16,25 @@
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "utils.hpp"
 #include <stb_image_write.h>
-// TODO what is this file used for? Seems unnecessary.
+
+constexpr Color_256 sky = {
+    .x = {0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f, 0.5f},
+    .y = {0.7f, 0.7f, 0.7f, 0.7f, 0.7f, 0.7f, 0.7f, 0.7f},
+    .z = {1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f, 1.f},
+};
+
+void update_colors(Color_256* curr_colors, const Color_256* new_colors,
+                                 __m256 update_mask);
+
+ Color_256 ray_cluster_colors(RayCluster* rays) ;
+
+// writes a color buffer of 32 Color values to an image buffer
+// uses non temporal writes to avoid filling data cache
+ void write_out_color_buf(const Color* color_buf, CharColor* img_buf,
+                                       uint32_t write_pos);
+
+ void render(CharColor* img_buf, const Vec3 cam_origin, uint32_t pix_offset);
+
+ void render_png();
+
+ void render_realtime();
