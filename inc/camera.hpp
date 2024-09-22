6171bf8ea29e28a490ee340111a120b8e6679c89
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-09-22 16:46:08 -0400

    moved camera methods to cpp

--- inc/camera.hpp
+++ inc/camera.hpp
@@ -2,52 +2,15 @@
 #include "types.hpp"
 #include <SDL_events.h>
 #include <SDL_keycode.h>
-#include <cstdint>
 
 class Camera {
 public:
-  Vec3 origin = {-1.2, 1, 5};
-  void register_key_event(SDL_Event e) {
+  Vec3 origin {-1.2, 1, 5};
 
-    uint32_t key = e.key.keysym.sym;
-
-    if (e.type == SDL_KEYDOWN) {
-      if (key == SDLK_w) {
-        velocity.z = -speed;
-      }
-      if (key == SDLK_s) {
-        velocity.z = speed;
-      }
-      if (key == SDLK_a) {
-        velocity.x = -speed;
-      }
-      if (key == SDLK_d) {
-        velocity.x = speed;
-      }
-    }
-    if (e.type == SDL_KEYUP) {
-      if (key == SDLK_w) {
-        velocity.z = 0;
-      }
-      if (key == SDLK_s) {
-        velocity.z = 0;
-      }
-      if (key == SDLK_a) {
-        velocity.x = 0;
-      }
-      if (key == SDLK_d) {
-        velocity.x = 0;
-      }
-    }
-  }
-
-  void update() {
-    origin.x += velocity.x;
-    origin.y += velocity.y;
-    origin.z += velocity.z;
-  }
+  void register_key_event(const SDL_Event e);
+  void update();
 
 private:
-  Vec3 velocity = {0, 0, 0};
-  const float speed = 0.01f;
+  Vec3 velocity {0, 0, 0};
+  static constexpr float speed = 0.01f;
 };
---
+++ src/camera.cpp
@@ -0,0 +1,45 @@
+#include "camera.hpp"
+
+void Camera::register_key_event(const SDL_Event e) {
+  const uint32_t key = e.key.keysym.sym;
+
+  if (e.type == SDL_KEYDOWN) {
+    switch (key) {
+    case SDLK_w:
+      velocity.z = -speed;
+      break;
+    case SDLK_s:
+      velocity.z = speed;
+      break;
+    case SDLK_a:
+      velocity.x = -speed;
+      break;
+    case SDLK_d:
+      velocity.x = speed;
+      break;
+    default:
+      break;
+    }
+  }
+  else if (e.type == SDL_KEYUP) {
+    switch (key) {
+    case SDLK_w:
+    case SDLK_s:
+      velocity.z = 0;
+      break;
+    case SDLK_a:
+    case SDLK_d:
+      velocity.x = 0;
+      break;
+    default:
+      break;
+    }
+  }
+}
+
+void Camera::update() {
+	// TODO origin += velocity
+  origin.x += velocity.x;
+  origin.y += velocity.y;
+  origin.z += velocity.z;
+}
