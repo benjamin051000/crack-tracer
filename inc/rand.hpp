1161583cfd8eb9ec44fb411ce217115b7368026f
Benjamin Wheeler <benjaminwheeler0510@gmail.com>
2024-10-05 11:08:19 -0400

    always inlined lcgrand

--- inc/rand.hpp
+++ inc/rand.hpp
@@ -3,15 +3,15 @@
 
 class LCGRand {
 public:
-  [[nodiscard]] inline Vec3_256 random_unit_vec() {
+  [[nodiscard, gnu::always_inline]] inline Vec3_256 random_unit_vec() {
     Vec3_256 rand_vec = rand_vec_in_cube();
     normalize(&rand_vec);
     return rand_vec;
   };
 
-  [[nodiscard]] inline float rand_in_range(float min, float max) {
-    float scale = lcg_rand() * rcp_rand_max;
-    float f = min + scale * (max - min);
+  [[nodiscard, gnu::always_inline]] inline float rand_in_range(const float min, const float max) {
+    const float scale = lcg_rand() * rcp_rand_max;
+    const float f = min + scale * (max - min);
     return f;
   }
 
@@ -29,27 +29,25 @@ public:
   }
 
 private:
-  static inline thread_local __m256i rseed_vec = comptime::init_rseed_arr();
-  static inline thread_local uint32_t rseed = 0;
+  static inline __m256i rseed_vec = comptime::init_rseed_arr();
+  static inline uint32_t rseed = 0;
   const __m256i r_a = _mm256_set1_epi32((uint32_t)11035152453u);
   const __m256i r_b = _mm256_set1_epi32(12345u);
   const __m256i rand_max_vec = _mm256_set1_epi32(RAND_MAX);
   static constexpr float rcp_rand_max = 1.f / RAND_MAX;
 
-  [[nodiscard]] inline Vec3_256 rand_vec_in_cube() {
-    float min = -1.0;
-    float max = 1.0;
+  [[nodiscard, gnu::always_inline]] inline Vec3_256 rand_vec_in_cube() {
+    constexpr float min = -1.0;
+    constexpr float max = 1.0;
 
-    Vec3_256 rand_vec = {
-        .x = rand_in_range_256(min, max),
-        .y = rand_in_range_256(min, max),
-        .z = rand_in_range_256(min, max),
+    return Vec3_256 {
+        rand_in_range_256(min, max),
+        rand_in_range_256(min, max),
+        rand_in_range_256(min, max),
     };
-
-    return rand_vec;
   }
 
-  [[nodiscard]] inline __m256i lcg_rand_256() {
+  [[nodiscard, gnu::always_inline]] inline __m256i lcg_rand_256() {
     rseed_vec = _mm256_mullo_epi32(rseed_vec, r_a);
     rseed_vec = _mm256_add_epi32(rseed_vec, r_b);
     rseed_vec = _mm256_and_si256(rseed_vec, rand_max_vec);
@@ -57,5 +55,5 @@ private:
   };
 
   // scalar versions of rand generation
-  [[nodiscard]] inline int lcg_rand() { return rseed = (rseed * 1103515245u + 12345u) & RAND_MAX; }
+  [[nodiscard, gnu::always_inline]] inline int lcg_rand() { return rseed = (rseed * 1103515245u + 12345u) & RAND_MAX; }
 };
